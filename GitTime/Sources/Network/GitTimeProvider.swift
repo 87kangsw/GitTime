704b13dd59a8d119b146b049636454950dcb406b
87kangsw <87kangsw@gmail.com>
2024-04-25 07:50:17 +0900

    bump SwiftyBeaver version

--- GitTime/Sources/Network/GitTimeProvider.swift
+++ GitTime/Sources/Network/GitTimeProvider.swift
@@ -30,12 +30,12 @@ class GitTimeProvider<Target: TargetType>: MoyaProvider<Target> {
             .asObservable()
             .do(onNext: { value in
                 let message = "SUCCESS: \(requestString) (\(value))"
-                log.debug(message, #file, #function, line: #line)
+                log.debug("\(message), \(#file), \(#function), line: \(#line)")
             }, onError: { [weak self] error in
                 if let response = (error as? MoyaError)?.response {
                   if let jsonObject = try? response.mapJSON(failsOnEmptyData: false) {
                     let message = "FAILURE: \(requestString) (\(response.statusCode))\n\(jsonObject)"
-                    log.warning(message, #file, #function, line: #line)
+                    log.warning("\(message), \(#file), \(#function), line: \(#line)")
                     if response.statusCode == 403 {
                         self?.showErrorMessageToast("API rate limit exceeded..\nPlease wait a moment. 🙏")
                     } else {
@@ -43,18 +43,18 @@ class GitTimeProvider<Target: TargetType>: MoyaProvider<Target> {
                     }
                   } else if let rawString = String(data: response.data, encoding: .utf8) {
                     let message = "FAILURE: \(requestString) (\(response.statusCode))\n\(rawString)"
-                    log.warning(message, #file, #function, line: #line)
+                    log.warning("\(message), \(#file), \(#function), line: \(#line)")
                   } else {
                     let message = "FAILURE: \(requestString) (\(response.statusCode))"
-                    log.warning(message, #file, #function, line: #line)
+                    log.warning("\(message), \(#file), \(#function), line: \(#line)")
                   }
                 } else {
                   let message = "FAILURE: \(requestString)\n\(error)"
-                  log.warning(message, #file, #function, line: #line)
+                  log.warning("\(message), \(#file), \(#function), line: \(#line)")
                 }
             }, onSubscribed: {
                 let message = "REQUEST: \(requestString)"
-                log.debug(message, #file, #function, line: #line)
+                log.debug("\(message), \(#file), \(#function), line: \(#line)")
             })
     }
     
